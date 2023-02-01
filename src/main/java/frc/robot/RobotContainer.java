ad40b6cc9e58efd4be8ce090696a55e7f565948a
RadTheCat13 <123786316+RadTheCat13@users.noreply.github.com>
2023-01-31 20:33:42 -0600

    add compressor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import frc.robot.commands.DriveManualCommand;
 import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -13,37 +15,49 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-
 
 public class RobotContainer {
   // Subsystems
   private final DriveTrain driveTrain = new DriveTrain();
 
-  //Controllers
-  private final XboxController controller =
-      new XboxController(0);
-  //Commands
+  // Pneumatics
+  private final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+
+  // Controllers
+  private final XboxController controller = new XboxController(0);
+  // Commands
   private final DriveManualCommand driveManualCommand = new DriveManualCommand(driveTrain, controller);
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-    //Set default commands for subsystems
+    // Set default commands for subsystems
     driveTrain.setDefaultCommand(driveManualCommand);
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
@@ -51,9 +65,10 @@ public class RobotContainer {
     new Trigger(driveTrain::exampleCondition)
         .onTrue(new DriveManualCommand(driveTrain, controller));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
-    //controller.().whileTrue(driveTrain.exampleMethodCommand());
+    // controller.().whileTrue(driveTrain.exampleMethodCommand());
   }
 
   /**
@@ -63,6 +78,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Commands.print( "No autonomous command configured.");
-  }}
-
+    return Commands.print("No autonomous command configured.");
+  }
+}
